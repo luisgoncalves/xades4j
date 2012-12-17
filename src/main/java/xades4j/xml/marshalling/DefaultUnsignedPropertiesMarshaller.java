8a64130b66a0451e9be646a6a5d19328292910fa
Hubert Kario <kario@wit.edu.pl>
2012-12-17 22:19:56 +0100

    add TimeStampValidationData marshaller
    Signed-off-by: Tiago Rossi <tiago.rossi@almg.gov.br>

--- src/main/java/xades4j/xml/marshalling/DefaultUnsignedPropertiesMarshaller.java
+++ src/main/java/xades4j/xml/marshalling/DefaultUnsignedPropertiesMarshaller.java
@@ -29,6 +29,7 @@ import xades4j.properties.data.CompleteRevocationRefsData;
 import xades4j.properties.data.RevocationValuesData;
 import xades4j.properties.data.SigAndRefsTimeStampData;
 import xades4j.properties.data.SignatureTimeStampData;
+import xades4j.properties.data.TimeStampValidationDataData;
 import xades4j.xml.bind.xades.ObjectFactory;
 import xades4j.xml.bind.xades.XmlUnsignedDataObjectPropertiesType;
 import xades4j.xml.bind.xades.XmlUnsignedPropertiesType;
@@ -61,6 +62,7 @@ class DefaultUnsignedPropertiesMarshaller
         super.putConverter(RevocationValuesData.class, new ToXmlRevocationValuesConverter());
         super.putConverter(AttributeRevocationValuesData.class, new ToXmlAttributeRevocationValuesConverter());
         super.putConverter(ArchiveTimeStampData.class, new ToXmlArchiveTimeStampConverter(algorithmsParametersMarshallingProvider));
+        super.putConverter(TimeStampValidationDataData.class, new ToXmlTimeStampValidationDataConverter());
         /* The CounterSignature property is marshalled directly using DOM because
          * it is easier that way (it is represented by a GenericDOMData instance).
          */
