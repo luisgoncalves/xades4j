67ecebd02cece15fa915cf039f72d4eebea945ee
nekkiy <hard-t@mail.ru>
2018-10-28 01:46:48 +0300

    [Issue-166] - added test for compare DN using X500NameStyleProvider set java test sources version to 1.7 improved test resources structure

--- src/test/java/xades4j/utils/Issue166Test.java
+++ src/test/java/xades4j/utils/Issue166Test.java
@@ -1,6 +1,6 @@
 package xades4j.utils;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import javax.security.auth.x500.X500Principal;
@@ -8,6 +8,8 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import xades4j.providers.X500NameStyleProvider;
+import xades4j.providers.impl.DefaultX500NameStyleProvider;
 
 /**
  * Investigation for https://github.com/luisgoncalves/xades4j/issues/166.
@@ -15,20 +17,23 @@ import org.junit.Test;
  */
 public class Issue166Test extends SignatureServicesTestBase
 {
-    String dnUtf8 = "2.5.4.97=#0c0f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
-    String dnPrintable = "2.5.4.97=#130f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
-    String dnPlain = "OID.2.5.4.97=VATES-A66721499, CN=UANATACA CA1 2016, OU=TSP-UANATACA, O=UANATACA S.A., L=Barcelona (see current address at www.uanataca.com/address), C=ES";
+    private String dnUtf8 = "2.5.4.97=#0c0f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
+    private String dnPrintable = "2.5.4.97=#130f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
+    private String dnPlain = "OID.2.5.4.97=VATES-A66721499, CN=UANATACA CA1 2016, OU=TSP-UANATACA, O=UANATACA S.A., L=Barcelona (see current address at www.uanataca.com/address), C=ES";
 
-    X509Certificate cert;
+    private X509Certificate cert;
+    private X500NameStyleProvider nameStyleProvider;
 
     @Before
     public void setUp() throws Exception
     {
         CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
         // Certificate includes the value of OID.2.5.4.97 as UTF8String
-        FileInputStream fis = new FileInputStream(toPlatformSpecificCertDirFilePath("issue166/EMPUBqscdA.cer"));
-        cert = (X509Certificate) certFactory.generateCertificate(fis);
-        fis.close();
+        try(InputStream is = getClass().getResourceAsStream("issue166/EMPUBqscdA.cer"))
+        {
+            cert = (X509Certificate) certFactory.generateCertificate(is);
+        }
+        nameStyleProvider= new DefaultX500NameStyleProvider();
     }
 
     @Test
@@ -116,4 +121,16 @@ public class Issue166Test extends SignatureServicesTestBase
 
         Assert.assertTrue(principal1.equals(principal2));
     }
+
+    @Test
+    public void compareWithNameStyleProvider()
+    {
+        X500Principal principal1 = new X500Principal(dnUtf8);
+        X500Principal principal2 = new X500Principal(dnPrintable);
+        X500Principal principal3 = new X500Principal(dnPlain);
+
+        Assert.assertTrue(nameStyleProvider.areEqual(principal1,principal2));
+        Assert.assertTrue(nameStyleProvider.areEqual(principal1,principal3));
+    }
+
 }
