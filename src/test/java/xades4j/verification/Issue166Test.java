ba1c1791c86482369289c0027006721155e814df
Luís Gonçalves <luis.fgoncalv@gmail.com>
2018-10-28 12:05:48 +0000

    Move Issue166Test to "verification" package

--- src/test/java/xades4j/verification/Issue166Test.java
+++ src/test/java/xades4j/verification/Issue166Test.java
@@ -1,4 +1,4 @@
-package xades4j.utils;
+package xades4j.verification;
 
 import java.io.InputStream;
 import java.security.cert.CertificateFactory;
@@ -8,8 +8,8 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import xades4j.providers.X500NameStyleProvider;
 import xades4j.providers.impl.DefaultX500NameStyleProvider;
+import xades4j.utils.SignatureServicesTestBase;
 
 /**
  * Investigation for https://github.com/luisgoncalves/xades4j/issues/166.
@@ -17,12 +17,12 @@ import xades4j.providers.impl.DefaultX500NameStyleProvider;
  */
 public class Issue166Test extends SignatureServicesTestBase
 {
-    private String dnUtf8 = "2.5.4.97=#0c0f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
-    private String dnPrintable = "2.5.4.97=#130f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
-    private String dnPlain = "OID.2.5.4.97=VATES-A66721499, CN=UANATACA CA1 2016, OU=TSP-UANATACA, O=UANATACA S.A., L=Barcelona (see current address at www.uanataca.com/address), C=ES";
+    private final String dnUtf8 = "2.5.4.97=#0c0f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
+    private final String dnPrintable = "2.5.4.97=#130f56415445532d413636373231343939,CN=UANATACA CA1 2016,OU=TSP-UANATACA,O=UANATACA S.A.,L=Barcelona (see current address at www.uanataca.com/address),C=ES";
+    private final String dnPlain = "OID.2.5.4.97=VATES-A66721499, CN=UANATACA CA1 2016, OU=TSP-UANATACA, O=UANATACA S.A., L=Barcelona (see current address at www.uanataca.com/address), C=ES";
 
     private X509Certificate cert;
-    private X500NameStyleProvider nameStyleProvider;
+    private DefaultX500NameStyleProvider nameStyleProvider;
 
     @Before
     public void setUp() throws Exception
@@ -33,7 +33,7 @@ public class Issue166Test extends SignatureServicesTestBase
         {
             cert = (X509Certificate) certFactory.generateCertificate(is);
         }
-        nameStyleProvider= new DefaultX500NameStyleProvider();
+        nameStyleProvider = new DefaultX500NameStyleProvider();
     }
 
     @Test
@@ -131,6 +131,7 @@ public class Issue166Test extends SignatureServicesTestBase
 
         Assert.assertTrue(nameStyleProvider.areEqual(principal1,principal2));
         Assert.assertTrue(nameStyleProvider.areEqual(principal1,principal3));
+        Assert.assertTrue(nameStyleProvider.areEqual(principal2,principal3));
     }
 
 }
